ed7a8da878099bef3553218ac778ba1b867e3a6b
agustinnava <agustinnava0@gmail.com>
2022-11-08 17:28:14 -0300

    descubri y hotel terminados

--- js/hotel.js
+++ js/hotel.js
@@ -1,13 +1,36 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems,{
-         setDefaultDate: true,
-         format: 'dddd dd mmmm yyyy',
-        });
+    var options = {
+      setDefaultDate: true,
+      format: 'dddd dd mmmm yyyy',
+      i18n: {months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+      monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+      weekdays: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+      weekdaysShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
+      selectMonths: true,
+      selectYears: 100,
+      today: 'Hoy',
+      clear: 'Limpiar',
+      close: 'Ok',
+      labelMonthNext: 'Siguiente mes',
+      labelMonthPrev: 'Mes anterior',
+      labelMonthSelect: 'Selecciona un mes',
+      labelYearSelect: 'Selecciona un año'}
+    }
+    var instances = M.Datepicker.init(elems, options);
   });
 
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.timepicker');
     var options;
     var instances = M.Timepicker.init(elems, options);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var options = {fullWidth: true,
+                   indicators: true,
+                   'duration': 200,
+                   'dist': 0,}
+    var instances = M.Carousel.init(elems, options);
   });
