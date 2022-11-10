879fa8b79dfea79e2f105c06064d02fbdd868b87
Matias Abuin <matias.abuin17@gmail.com>
2022-11-10 18:52:58 -0300

    guias

--- js/carousel.js
+++ js/carousel.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.carousel');
-  var options = {'padding':20, 'dist': -20};
+  var options = {'padding':20, 'dist': -20, 'shift': 5};
   var instances = M.Carousel.init(elems, options);
 });
 
