da391baa695ce75565fad8f26fd87c1ad19413ec
Matias Abuin <matias.abuin17@gmail.com>
2022-11-11 14:29:47 -0300

    cambios en clase

--- js/hotel.js
+++ js/hotel.js
@@ -26,11 +26,3 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Timepicker.init(elems, options);
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var options = {fullWidth: true,
-                   indicators: true,
-                   'duration': 200,
-                   'dist': 0,}
-    var instances = M.Carousel.init(elems, options);
-  });
