27c2a1a60167f4319d9cec4bc223719c8608997b
Matias Abuin <matias.abuin17@gmail.com>
2022-11-10 22:12:18 -0300

    select guias

---
+++ js/guias.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var options;
+    var instances = M.FormSelect.init(elems, options);
+  });
+
